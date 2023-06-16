c4caf26bfae0fe9e1bbfb439b4646c633cfc55be
Cai <13110818005@qq.com>
2023-06-16 23:50:38 +0800

    [RemoveItem]修复无法在线移除物品的BUG

--- MainPlugin.cs
+++ MainPlugin.cs
@@ -8,6 +8,7 @@ using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.DB;
 using System.Threading;
+using Terraria.ID;
 
 [ApiVersion(2, 1)]
 public class MainPlugin : TerrariaPlugin
@@ -31,9 +32,9 @@ public class MainPlugin : TerrariaPlugin
     public override void Initialize()
     {
         Commands.ChatCommands.Add(new Command("itemsearch.cmd", ItemSearchCmd, "searchitem", "si", "查找物品"));
-        Commands.ChatCommands.Add(new Command("itemsearch.cmd", ChestSearchCmd, "searchchest", "sc", "查找箱子"));
-        Commands.ChatCommands.Add(new Command("itemsearch.cmd", TpSearchCmd, "tpchest", "tpc", "传送箱子"));
-        Commands.ChatCommands.Add(new Command("itemsearch.cmd", InfoSearchCmd, "chestinfo", "ci", "箱子信息"));
+        Commands.ChatCommands.Add(new Command("itemsearch.chest", ChestSearchCmd, "searchchest", "sc", "查找箱子"));
+        Commands.ChatCommands.Add(new Command("itemsearch.chesttp", TpSearchCmd, "tpchest", "tpc", "传送箱子"));
+        Commands.ChatCommands.Add(new Command("itemsearch.chestinfo", InfoSearchCmd, "chestinfo", "ci", "箱子信息"));
         Commands.ChatCommands.Add(new Command("itemsearch.tpall", TpAllPlayer, "tpall", "传送所有人"));
         Commands.ChatCommands.Add(new Command("itemsearch.tpall", TpAllChest, "tpallchest", "tpallc", "传送所有箱子"));
         Commands.ChatCommands.Add(new Command("itemsearch.rci", RemoveItemChest, "removechestitem", "rci", "删除箱子物品"));
@@ -70,36 +71,204 @@ public class MainPlugin : TerrariaPlugin
         int item = itemByIdOrName[0].netID;
         TSPlayer player = new(-1);
         PlayerData? playerdata;
+        int count = 0;
         if (TSPlayer.FindByNameOrID(acc.Name).FirstOrDefault() != null)
         {
-            var plr = TSPlayer.FindByNameOrID(acc.Name).FirstOrDefault();
-            //改成for()循环
+            //Item[] armor;
+            //Item[] dye;
+            //Item[] miscEquips;
+            //Item[] miscDyes;
+            var plr = TSPlayer.FindByNameOrID(acc.Name).FirstOrDefault()!;
             for (int i = 0; i < plr.TPlayer.inventory.Length; i++)
             {
                 if (plr.TPlayer.inventory[i].netID == item)
                 {
-                    plr.TPlayer.inventory[i].netID = 0;
-                    //发5号包更新背包
+                    count += plr.TPlayer.inventory[i].stack;
+                    plr.TPlayer.inventory[i].SetDefaults(0);
                     NetMessage.SendData(5, -1, -1, null, plr.Index, i);
 
                 }
             }
+            for (int i = 0; i < plr.TPlayer.armor.Length; i++)
+            {
+                if (plr.TPlayer.armor[i].netID == item)
+                {
+                    count += plr.TPlayer.armor[i].stack;
+                    plr.TPlayer.armor[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Armor0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.dye.Length; i++)
+            {
+                if (plr.TPlayer.dye[i].netID == item)
+                {
+                    count += plr.TPlayer.dye[i].stack;
+                    plr.TPlayer.dye[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Dye0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.miscEquips.Length; i++)
+            {
+                if (plr.TPlayer.miscEquips[i].netID == item)
+                {
+                    count += plr.TPlayer.miscEquips[i].stack;
+                    plr.TPlayer.miscEquips[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Misc0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.miscDyes.Length; i++)
+            {
+                if (plr.TPlayer.miscDyes[i].netID == item)
+                {
+                    count += plr.TPlayer.miscEquips[i].stack;
+                    plr.TPlayer.miscEquips[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.MiscDye0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.bank.item.Length; i++)
+            {
+                if (plr.TPlayer.bank.item[i].netID == item)
+                {
+                    count += plr.TPlayer.bank.item[i].stack;
+                    plr.TPlayer.bank.item[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Bank1_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.bank2.item.Length; i++)
+            {
+                if (plr.TPlayer.bank2.item[i].netID == item)
+                {
+                    count += plr.TPlayer.bank2.item[i].stack;
+                    plr.TPlayer.bank2.item[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Bank2_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.bank3.item.Length; i++)
+            {
+                if (plr.TPlayer.bank3.item[i].netID == item)
+                {
+                    count += plr.TPlayer.bank3.item[i].stack;
+                    plr.TPlayer.bank3.item[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Bank3_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.bank4.item.Length; i++)
+            {
+                if (plr.TPlayer.bank4.item[i].netID == item)
+                {
+                    count += plr.TPlayer.bank4.item[i].stack;
+                    plr.TPlayer.bank4.item[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Bank4_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.Loadouts[0].Armor.Length; i++)
+            {
+                if (plr.TPlayer.Loadouts[0].Armor[i].netID == item)
+                {
+                    count += plr.TPlayer.Loadouts[0].Armor[i].stack;
+                    plr.TPlayer.Loadouts[0].Armor[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Loadout1_Armor_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.Loadouts[1].Armor.Length; i++)
+            {
+                if (plr.TPlayer.Loadouts[1].Armor[i].netID == item)
+                {
+                    count += plr.TPlayer.Loadouts[1].Armor[i].stack;
+                    plr.TPlayer.Loadouts[1].Armor[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Loadout2_Armor_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.Loadouts[2].Armor.Length; i++)
+            {
+                if (plr.TPlayer.Loadouts[2].Armor[i].netID == item)
+                {
+                    count += plr.TPlayer.Loadouts[2].Armor[i].stack;
+                    plr.TPlayer.Loadouts[2].Armor[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Loadout3_Armor_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.Loadouts[0].Dye.Length; i++)
+            {
+                if (plr.TPlayer.Loadouts[0].Dye[i].netID == item)
+                {
+                    count += plr.TPlayer.Loadouts[0].Dye[i].stack;
+
+                    plr.TPlayer.Loadouts[0].Dye[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Loadout1_Dye_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.Loadouts[1].Dye.Length; i++)
+            {
+                if (plr.TPlayer.Loadouts[1].Dye[i].netID == item)
+                {
+                    count += plr.TPlayer.Loadouts[1].Dye[i].stack;
+                    plr.TPlayer.Loadouts[1].Dye[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Loadout2_Dye_0 + i);
+
+                }
+            }
+            for (int i = 0; i < plr.TPlayer.Loadouts[2].Dye.Length; i++)
+            {
+                if (plr.TPlayer.Loadouts[2].Dye[i].netID == item)
+                {
+                    count += plr.TPlayer.Loadouts[2].Dye[i].stack;
+                    plr.TPlayer.Loadouts[2].Dye[i].SetDefaults(0);
+                    NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.Loadout3_Dye_0 + i);
+
+                }
+            }
+            if (plr.TPlayer.trashItem.netID == item)
+            {
+                count += plr.TPlayer.trashItem.stack;
+                plr.TPlayer.trashItem.SetDefaults(0);
+                NetMessage.SendData(5, -1, -1, null, plr.Index, PlayerItemSlotID.TrashItem);
+            }
+            //Item[] array = player.TPlayer.inventory; 1
+            //Item[] armor = player.TPlayer.armor;1
+            //Item[] dye = player.TPlayer.dye;1
+            //Item[] miscEquips = player.TPlayer.miscEquips;1
+            //Item[] miscDyes = player.TPlayer.miscDyes;1
+            //Item[] item = player.TPlayer.bank.item;1
+            //Item[] item2 = player.TPlayer.bank2.item;1
+            //Item[] item3 = player.TPlayer.bank3.item;1
+            //Item[] item4 = player.TPlayer.bank4.item;1
+            //Item trashItem = player.TPlayer.trashItem;
+            //Item[] armor2 = player.TPlayer.Loadouts[0].Armor;
+            //Item[] dye2 = player.TPlayer.Loadouts[0].Dye;
+            //Item[] armor3 = player.TPlayer.Loadouts[1].Armor;
+            //Item[] dye3 = player.TPlayer.Loadouts[1].Dye;
+            //Item[] armor4 = player.TPlayer.Loadouts[2].Armor;
+            //Item[] dye4 = player.TPlayer.Loadouts[2].Dye;
         }
         else
         {
             try
             {
                 playerdata = TShock.CharacterDB.GetPlayerData(player, acc.ID);
-                
+
                 //改成for()循环
                 for (int i = 0; i < playerdata.inventory.Length; i++)
                 {
                     if (playerdata.inventory[i].NetId == item)
                     {
-                        playerdata.inventory[i]  = new(0,0,0);
+                        count += playerdata.inventory[i].Stack;
+                        playerdata.inventory[i] = new(0, 0, 0);
                     }
                 }
-                
+
                 //更新数据库背包
                 TShock.CharacterDB.database.Query("UPDATE tsCharacter SET Inventory = @0 WHERE Account = @1", string.Join("~", playerdata.inventory), acc.ID);
 
@@ -110,7 +279,7 @@ public class MainPlugin : TerrariaPlugin
                 return;
             }
         }
-        args.Player.SendSuccessMessage($"已移除玩家{acc.Name}的所有{TShock.Utils.ItemTag(new() { netID = item,stack=1,prefix=0})}");
+        args.Player.SendSuccessMessage($"已移除玩家{acc.Name}的{TShock.Utils.ItemTag(new() { netID = item, stack = 1, prefix = 0 })}X{count}");
         return;
 
 
@@ -170,7 +339,7 @@ public class MainPlugin : TerrariaPlugin
         {
             itemStr = "空箱子";
         }
-        args.Player.SendSuccessMessage($"箱子中的所以{TShock.Utils.ItemTag(new() { netID = item, stack = 1, prefix = 0 })}已被移除\n" +
+        args.Player.SendSuccessMessage($"箱子中的所有{TShock.Utils.ItemTag(new() { netID = item, stack = 1, prefix = 0 })}已被移除\n" +
             $"箱子ID:{args.Parameters[0]}\n" +
             $"坐标:({chest.x},{chest.y})\n" +
             $"名字:{(string.IsNullOrEmpty(chest.name) ? "无名箱子" : chest.name)}\n" +
